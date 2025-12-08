3d0795bce9d8fcbd11c41b0f71f00df1ed59f02c
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-08 17:32:18 +0600

    About Page banner added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className='w-full max-w-[1440px] mx-auto h-auto px-4 mb-10 font-inter'>
+            <div className='bg-[url(../../src/assets/about.jpg)] bg-cover bg-center bg-no-repeat w-full h-[400px] flex flex-col justify-center'>
+                <p className='font-playfair font-bold text-[40px] mb-[5px] text-white text-center'>About ThreadFlow</p>
+                <p className='text-[#666666] text-center'>Revolutionizing garment production management with innovative technology and seamless workflow solutions.</p>
+            </div>
         </div>
     );
 };
