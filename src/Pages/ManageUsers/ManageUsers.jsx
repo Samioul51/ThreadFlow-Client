9e2a2d6457db9135ca640ecde9dc644c17540097
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-13 21:46:00 +0600

    Added filter on user management page

--- src/Pages/ManageUsers/ManageUsers.jsx
+++ src/Pages/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,6 @@
 import React, { use, useState } from 'react';
 import toast from 'react-hot-toast';
+import { IoIosArrowDropdown } from 'react-icons/io';
 
 const userPromise = fetch("http://localhost:3000/users").then(res => res.json());
 
@@ -11,6 +12,20 @@ const ManageUsers = () => {
 
     const [selectedUser, setSelectedUser] = useState(null);
 
+    const categories = ["All Role", "Buyer", "Manager", "Admin"];
+    const allStatus = ["All Status", "Pending", "Suspended", "Approved"];
+
+    const [category, setCategory] = useState("All Role");
+    const [status, setStatus] = useState("All Status");
+    const [search, setSearch] = useState("");
+
+    const filteredUsers = users.filter(p => {
+        const matchCategory = category === "All Role" ? true : p.role.toLowerCase() === category.toLowerCase();
+        const matchStatus = status === "All Status" ? true : p.roleStatus.toLowerCase() === status.toLowerCase();
+        const matchSearch = p.name.toLowerCase().includes(search.toLowerCase()) || p.email.toLowerCase().includes(search.toLowerCase()) || p.role.toLowerCase().includes(search.toLowerCase()) || p.roleStatus.toLowerCase().includes(search.toLowerCase());
+        return matchCategory && matchStatus && matchSearch;
+    });
+
     const handleOpenModal = (user) => {
         setSelectedUser(user);
         document.getElementById("update_user_modal").showModal();
@@ -21,18 +36,18 @@ const ManageUsers = () => {
         setSelectedUser(null);
     }
 
-    const handleUserUpdate =async (e) => {
+    const handleUserUpdate = async (e) => {
         e.preventDefault();
-        if(!selectedUser)
+        if (!selectedUser)
             return;
-        const form=e.target;
+        const form = e.target;
 
-        const roleStatus=form.roleStatus.value;
-        const feedback=form?.feedback?.value || "";
-        
-        const updatedInfo={
-            roleStatus:roleStatus,
-            feedback:feedback
+        const roleStatus = form.roleStatus.value;
+        const feedback = form?.feedback?.value || "";
+
+        const updatedInfo = {
+            roleStatus: roleStatus,
+            feedback: feedback
         };
 
         const res = await fetch(`http://localhost:3000/users/${selectedUser._id}`, {
@@ -43,7 +58,7 @@ const ManageUsers = () => {
             body: JSON.stringify(updatedInfo),
         });
 
-        if(res.ok){
+        if (res.ok) {
             toast.success("User updated!");
             handleCloseModal();
             setSelectedUser(null);
@@ -56,8 +71,27 @@ const ManageUsers = () => {
     return (
         <div className='py-5 mx-10 mt-10 flex flex-col items-center min-h-screen bg-white font-inter'>
             <p className='font-playfair text-black text-3xl font-bold text-center mb-5'>MANAGE USERS</p>
+            <div className='w-full max-w-full p-[16px] box-border flex justify-between items-center'>
+                <div className="dropdown dropdown-start">
+                    <div tabIndex={0} role="button" className="btn m-1">{category} <IoIosArrowDropdown /></div>
+                    <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        {
+                            categories.map(c => <li key={c}><a onClick={() => setCategory(c)}>{c}</a></li>)
+                        }
+                    </ul>
+                </div>
+                <div className="dropdown dropdown-start">
+                    <div tabIndex={0} role="button" className="btn m-1">{status} <IoIosArrowDropdown /></div>
+                    <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        {
+                            allStatus.map(c => <li key={c}><a onClick={() => setStatus(c)}>{c}</a></li>)
+                        }
+                    </ul>
+                </div>
+                <input type="text" placeholder="Search" className="input input-primary" onChange={(e) => setSearch(e.target.value)} />
+            </div>
             {
-                users.length > 0 ?
+                filteredUsers.length > 0 ?
                     <div className="w-full overflow-x-auto">
                         <table className="table">
                             {/* head */}
@@ -74,7 +108,7 @@ const ManageUsers = () => {
 
                             <tbody>
                                 {
-                                    users?.map((user, index) => (
+                                    filteredUsers?.map((user, index) => (
                                         <tr key={index}>
                                             <td>
                                                 {index + 1}
