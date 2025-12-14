dd36e3116bab1ebae5ecf3d649a5779e5045f4e6
Samioul51 <akmsamioulislam51@gmail.com>
2025-12-14 22:06:37 +0600

    Pagination implemented in All Products page with pagewise filtering

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,14 +1,26 @@
-import React, { use, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from '../../Components/ProductCard/ProductCard';
 import { IoIosArrowDropdown } from 'react-icons/io';
 
-const productPromise = fetch("http://localhost:3000/products").then(res => res.json());
-
 const Products = () => {
-    const result = use(productPromise);
-    const products = result.data;
 
-    console.log(products);
+    const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(0);
+
+    const limit = 9;
+
+    useEffect(() => {
+        const skip = (page - 1) * limit;
+
+        fetch(`http://localhost:3000/products?limit=${limit}&skip=${skip}`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data.data);
+                setTotal(data.total);
+            });
+    }, [page])
+    // console.log(products);
 
     const categories = ["All", "Shirt", "Pant", "Jacket", "Accessories"];
 
@@ -20,6 +32,8 @@ const Products = () => {
         const matchTitle = p.productName.toLowerCase().includes(searchTitle.toLowerCase());
         return matchCategory && matchTitle;
     });
+
+    const totalPages = Math.ceil(total / limit);
 
     return (
         <div className='w-full max-w-[1440px] mx-auto h-auto mb-10 mt-16'>
@@ -49,8 +63,41 @@ const Products = () => {
                         <div className='w-full max-w-[1440px] flex justify-center items-center h-[50vh]'>
                             <p className='font-playfair text-2xl text-center font-bold text-black'>NO PRODUCT FOUND!</p>
                         </div>
-                        
+
                     )
+            }
+
+            {/* Pagination */}
+            {
+                totalPages > 1 && (
+                    <div className="flex justify-center mt-8 gap-2">
+                        <button
+                            className="btn btn-sm"
+                            disabled={page === 1}
+                            onClick={() => setPage(page - 1)}
+                        >
+                            Prev
+                        </button>
+
+                        {[...Array(totalPages).keys()].map(num => (
+                            <button
+                                key={num}
+                                onClick={() => setPage(num + 1)}
+                                className={`btn btn-sm ${page === num + 1 ? "btn-active" : ""}`}
+                            >
+                                {num + 1}
+                            </button>
+                        ))}
+
+                        <button
+                            className="btn btn-sm"
+                            disabled={page === totalPages}
+                            onClick={() => setPage(page + 1)}
+                        >
+                            Next
+                        </button>
+                    </div>
+                )
             }
         </div>
     );
